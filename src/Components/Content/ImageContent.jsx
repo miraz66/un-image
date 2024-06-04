8d72455193dbab3ec14a1debaba1722f0c4cc1a9
Mirajul Islam <mirajulislam0509@gmail.com>
2024-06-04 11:52:11 +0600

    Image Content

--- src/Components/Content/ImageContent.jsx
+++ src/Components/Content/ImageContent.jsx
@@ -9,7 +9,7 @@ const ImageContent = () => {
     <div>
       <div className="w-8/12 m-auto pt-10">
         <div className="flex gap-4 overflow-hidden">
-          <div className="w-full bg-white grid grid-cols-1 gap-y-4 ">
+          {/* <div className="w-full bg-white grid grid-cols-1 gap-y-4 ">
             {ImgContent1.contant1.map((status) => {
               return <ImageLi images={status} />;
             })}
@@ -25,7 +25,7 @@ const ImageContent = () => {
             {ImgContent1.contant3.map((status) => {
               return <ImageLi images={status} />;
             })}
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
